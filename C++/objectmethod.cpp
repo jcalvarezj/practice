70aca260e7946ff397d6aa100c448ad6aab45a22
Juan Alvarez <jcalvarezj@gmail.com>
2019-10-07 16:56:32 -0500

    Added object array example to example of passing objects by method

--- C++/objectmethod.cpp
+++ C++/objectmethod.cpp
@@ -1,6 +1,7 @@
 /*
- * This is an example of how to return objects in methods. Short example, so the
- * class is defined in this file (not following best practices).
+ * This is an example of how to return objects in methods and some ways to
+ * instantiate objects of a type. Short example, so the class is defined in this
+ * file (not following best practices of separate files).
  *
  * Based on John Purcell's examples (www.caveofprogramming.com)
  *
@@ -8,6 +9,8 @@
  */
 #include <iostream>
 #include <sstream>
+
+const int NANIMALS = 3; // Remember modifying createAnimals() when changing it
 
 class Animal {
 	private:
@@ -48,10 +51,25 @@ Animal * createAnimal() {
 	return aNew;
 }
 
+Animal * createAnimals() {
+	Animal * animals = new Animal[NANIMALS];
+	animals[0].setName("Bird");
+	animals[1].setName("Lizard");
+	animals[2].setName("Wombat");
+	return animals;
+}
+
+void printArray(Animal array[], int n) {
+	std::cout << "There are " << n << " animals:" << std::endl;
+
+	for(int i=0; i<n; i++) {
+		std::cout << "Animal at " << i << " is " << array[i].getName() << std::endl;
+	}
+}
+
 int main(int argc, char ** args) {
 	
 	Animal * a = new Animal("Pepe");
-
 	a->speak();
 
 	Animal b = * a;
@@ -59,10 +77,14 @@ int main(int argc, char ** args) {
 	delete a;
 
 	Animal * c = createAnimal();
-
 	c->speak();
-
 	delete c;
+
+	Animal * d = createAnimals();
+
+	printArray(d,NANIMALS);
+
+	delete [] d;
 
 	return 0;
 }
