aab0a5c2f1d36b7358017d4caac8a7afc5f74ede
Juan Alvarez <jcalvarezj@gmail.com>
2019-10-09 18:49:41 -0500

    Finished Java FizzBuzz TDD example

--- Java/FizzBuzz.java
+++ Java/FizzBuzz.java
@@ -1,21 +1,29 @@
 /*
  * This is a small program that implements unit tests with JUnit
- * Based on https://medium.com/@pelensky/java-tdd-with-junit-without-using-an-ide-cd24d38adff
+ * Based on https://medium.com/@pelensky/java-tdd-with-junit-without-using-an-id
+ * e-cd24d38adff
  */
 
 /*
  * This class represents the FizzBuzz class; it works as follows:
- * 1. If the number is divisible by 3, the app returns “Fizz”,
- * 2. if the number is divisible by 5, the app returns “Buzz”,
- * 3. if the number is divisible by both three and five, the app returns “FizzBuzz”,
- * finally, if the number is not divisible by 3 or 5, the app returns the number.
+ * 1. If the number is divisible by 3, the program returns “Fizz”,
+ * 2. if the number is divisible by 5, the program returns “Buzz”,
+ * 3. if the number is divisible by both three and five, the program returns
+ * “FizzBuzz”,
+ * Finally, if the number is not divisible by 3 or 5, the program returns the
+ * number.
  */
 public class FizzBuzz {
 	
 	public String getResult(int number) {
-		return String.valueOf(number);
+		if(number%3 == 0 && number%5 == 0)
+			return "FizzBuzz";
+		else if(number%3 == 0)
+			return "Fizz";
+		else if(number%5 == 0)
+			return "Buzz";
+		else
+			return String.valueOf(number);
 	}
-
-	//TODO Use TDD to complete FizzBuzz
 
 }
--- Java/FizzBuzzTest.java
+++ Java/FizzBuzzTest.java
@@ -1,6 +1,7 @@
 /*
  * This is a small program that implements unit tests with JUnit
- * Based on https://medium.com/@pelensky/java-tdd-with-junit-without-using-an-ide-cd24d38adff
+ * Based on https://medium.com/@pelensky/java-tdd-with-junit-without-using-an-id
+ * e-cd24d38adff
  * Requires JUnit JAR file on same directory 
  * Compile and run accordingly to the JUnit version
  * Compile with javac -cp .:junit-4.10.jar FizzBuzzTest.java
@@ -26,7 +27,7 @@ public class FizzBuzzTest {
 	}
 
 	/*
-	 * This method tests if FizzBuzz's getResult returns "2" given a 2
+	 * This method tests if FizzBuzz's getResult returns "2" when given a 2
 	 */
 	@Test
 	public void testGetResult_returnTwoIfGivenTwo() {
@@ -34,4 +35,39 @@ public class FizzBuzzTest {
 		String result = fb.getResult(2);
 		assertEquals(dec+"IT SHOULD RETURN \"2\"!!"+dec,"2",result);
 	}
+
+	/*
+	 * This method tests if FizzBuzz's getResult returns "Fizz" when given a
+	 * multiple of 3
+	 */
+	@Test
+	public void testGetResult_returnFizzIfGivenMultipleOfThree() {
+		FizzBuzz fb = new FizzBuzz();
+		String result = fb.getResult(3);
+		assertEquals(dec+"IT SHOULD RETURN \"Fizz\"!!"+dec,"Fizz",result);
+	}
+
+	/*
+	 * This method tests if FizzBuzz's getResult returns "Buzz" when given a
+	 * multiple of 5
+	 */
+	@Test
+	public void testGetResult_returnBuzzIfGivenMultipleOfFive() {
+		FizzBuzz fb = new FizzBuzz();
+		String result = fb.getResult(5);
+		assertEquals(dec+"IT SHOULD RETURN \"Buzz\"!!"+dec,"Buzz",result);
+	}
+
+	/*
+	 * This method tests if FizzBuzz's getResult returns "FizzBuzz" when given a
+	 * multiple of both 3 and 5
+	 */
+	@Test
+	public void testGetResult_returnBuzzIfGivenMultipleOfThreeAndFive() {
+		FizzBuzz fb = new FizzBuzz();
+		String result = fb.getResult(15);
+		assertEquals(dec+"IT SHOULD RETURN \"FizzBuzz\"!!"+dec,"FizzBuzz",
+			result);
+	}
+
 }
