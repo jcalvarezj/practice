e8bfe40f6fd14d4f89c404028c7cee48b8a74c94
Juan Alvarez <jcalvarezj@gmail.com>
2019-10-16 18:21:27 -0500

    Hello World example of NodeJS Express web server

---
+++ nodeJS/helloWorld.js
@@ -0,0 +1,21 @@
+/*
+ * NodeJS Express Server test
+ */
+
+var express = require("express");
+var application = express();
+
+application.get("/", hello);
+application.get("/asdf",asdf);
+
+function hello(request, response) {
+	response.send("<p>Hello world</p>");
+}
+
+function asdf(request, response) {
+	response.send("<a href='https://www.youtube.com/watch?v=CWBnMBz-zZU'>"
+			+"ASDF MOVIE</a>");
+}
+
+//application.listen(8080,"192.168.1.63");
+application.listen(8080);
