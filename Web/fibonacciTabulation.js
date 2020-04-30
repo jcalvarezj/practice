5a47ef049b257554ef8e1b6b5945d750e002410e
J. Alvarez <jcalvarezj@gmail.com>
2020-04-30 10:36:55 -0500

    Added basic JS example of the Fibonacci sequence using the bottom-up dynamic programming scheme

---
+++ Web/fibonacciTabulation.js
@@ -0,0 +1,16 @@
+function fibonacciTabulation(n) {
+	const results = new Array(n).fill(0);  
+	results[0] = 0;  
+
+	if (n >= 1) {
+		results[1] = 1;
+
+		for (let i = 2; i <= n; i++)
+			results[i] = results[i-1] + results[i-2];
+	}
+
+	return results[n];
+}
+
+for (let i = 0; i < 7; i++)
+    console.log(fibonacciTabulation(i))
