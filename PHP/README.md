e4a546a6f1923ad9683d1ef4576f11edef702135
J. Alvarez <jcalvarezj@gmail.com>
2020-07-30 20:37:31 -0500

    Add README and improved code

---
+++ PHP/README.md
@@ -0,0 +1,10 @@
+# PHP
+
+This folder contains some exercises I solved using the PHP programming language.
+
+- [Basic Object Oriented Programming example](/PHP/ejemploClases/)
+- [Examples from AprenderAProgramar [Spanish]](/PHP/ejemplosAprenderAProgramar/)
+- [Basic MVC example [Spanish]](/PHP/miPrimerMVC/)
+- [Basic PHP program exploring functions [Spanish]](/PHP/miPrimerProyectoPHP/)
+- [REST API example](/PHP/REST_example/)
+- [Short JSON retrieval from URL example](/PHP/basicJSON.php)
--- PHP/ejemploClases/index.php
+++ PHP/ejemploClases/index.php
@@ -14,22 +14,22 @@ require "Car.php";
 
 echo "<b>Ejemplo muy basico de Orientacion a Objetos</b><br/><br/>";
 	
-$cuenta = new Account("Andres Herrera", 8888, "a@email.com", "1234");
+$account = new Account("Andres Herrera", 8888, "a@email.com", "1234");
 
-echo "<b>Datos de Cuenta:</b> ".$cuenta->toString()."<br/>";
+echo "<b>Datos de Cuenta:</b> ".$account->toString()."<br/>";
 
-$carro = new Car();
-$carro->driver = $cuenta;
-$carro->license = "AMQ123";
-$carro->passenger = 4;
+$car = new Car();
+$car->driver = $account;
+$car->license = "AMQ123";
+$car->passenger = 4;
 
-$info = $carro->driver->toString();
+$info = $car->driver->toString();
 
-echo generarMensaje("Datos del carro","$info maneja $carro->license");
+echo generarMensaje("Datos del car","$info maneja $car->license");
 
 
-function generarMensaje($titulo, $mensaje) {
-	return "<p><b>$titulo</b> $mensaje<br/></p>";
+function generarMensaje($title, $message) {
+	return "<p><b>$title</b> $message<br/></p>";
 }
 ?>
 		</div>
