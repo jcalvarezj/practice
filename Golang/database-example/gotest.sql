abc14ff6c860c21f139e6cb6898770d1e092c798
J. Alvarez <jcalvarezj@gmail.com>
2021-03-14 10:01:18 -0500

    Fix problematic collation on SQL script

--- Golang/database-example/gotest.sql
+++ Golang/database-example/gotest.sql
@@ -30,7 +30,7 @@ CREATE TABLE `user` (
   `sex` char(1) DEFAULT NULL,
   `biography` text,
   PRIMARY KEY (`pk_username`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
